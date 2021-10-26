34bda1f4b2494aa81caf0865908eb57ad629d78d
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-10-26 14:10:31 +0200

    Création du contrôleur

--- post.php
+++ post.php
@@ -5,8 +5,6 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
     $post = getPost($_GET['id']);
     $comments = getComments($_GET['id']);
     require('postView.php');
-}
-else {
+} else {
     echo 'Erreur : aucun identifiant de billet envoyé';
 }
-
