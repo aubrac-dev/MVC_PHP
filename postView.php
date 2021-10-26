44fef50db50984867058ee0b7f667dfbf308642b
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-10-26 14:11:22 +0200

    Création de la vue

--- postView.php
+++ postView.php
@@ -26,6 +26,7 @@
     <h2>Commentaires</h2>
 
     <?php
+
     while ($comment = $comments->fetch()) {
     ?>
         <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
