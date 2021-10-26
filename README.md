c42dcb0f44dc0c4cc6aee3e6af915375a7ea141c
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-10-26 13:49:21 +0200

    Le code amélioré

--- README.md
+++ README.md
@@ -2,10 +2,33 @@
 
 MVC, la Programmation Orientée Objet (POO) 
 
-On y reviendra, mais sachez que ces 3 fichiers forment la base d'une structure MVC (Modèle - Vue - Contrôleur):
+Notre code est maintenant découpé en 3 fichiers :
 
-Le modèle traite les données (modele.php)
+Un pour le traitement PHP : il récupère les données de la base. On l'appelle le modèle.
 
-La vue affiche les informations (affichageAccueil.php)
+Un pour l'affichage : il affiche les informations dans une page HTML. On l'appelle la vue.
 
-Le contrôleur fait le lien entre les deux (index.php)
+Un pour faire le lien entre les deux : on l'appelle le contrôleur.
+
+Cette structure est bien, mais nous allons faire quelques améliorations cosmétiques. Ce ne sont peut-être que des détails pour vous, mais ça fera une vraie différence à la fin.
+
+Voici ce que nous allons améliorer :
+
+L'anglais
+
+La balise de fermeture PHP
+
+L'utilisation de short open tags
+
+L'indentation
+
+
+--
+
+Enfin, ce n'est pas obligatoire mais j'ai utilisé les "short open tags" en PHP pour faciliter la lisibilité du code. Ainsi, par exemple :
+
+<?php echo htmlspecialchars($data['title']); ?>
+... est devenu :
+
+<?= htmlspecialchars($data['title']) ?>
+Cela permet d'éviter d'avoir à écrire echo quand on souhaite juste afficher une variable. Le but est d'être plus lisible dans la vue en enlevant le maximum de code PHP là-dedans (même si on ne peut pas tout enlever).
--- init_db.sql
+++ init_db.sql
@@ -18,12 +18,16 @@
 CREATE DATABASE blog;
 USE blog;
 
+
+
+
+
 -- phpMyAdmin SQL Dump
 -- version 4.7.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le :  jeu. 12 oct. 2017 à 18:35
+-- Généré le :  lun. 18 sep. 2017 à 18:27
 -- Version du serveur :  5.6.35
 -- Version de PHP :  7.1.6
 
@@ -37,27 +41,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `test`
+-- Base de données :  `blog`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `billets`
+-- Structure de la table `posts`
 --
 
-CREATE TABLE `billets` (
+CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
-  `titre` varchar(255) NOT NULL,
-  `contenu` text NOT NULL,
-  `date_creation` datetime NOT NULL
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `creation_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `billets`
+-- Déchargement des données de la table `posts`
 --
 
-INSERT INTO `billets` (`id`, `titre`, `contenu`, `date_creation`) VALUES
+INSERT INTO `posts` (`id`, `title`, `content`, `creation_date`) VALUES
 (1, 'Bienvenue sur mon blog !', 'Je vous souhaite à toutes et à tous la bienvenue sur mon blog qui parlera de... PHP bien sûr !', '2017-09-18 16:28:41'),
 (2, 'Le PHP à la conquête du monde !', 'C\'est officiel, l\'éléPHPant a annoncé à la radio hier soir \"J\'ai l\'intention de conquérir le monde !\".\r\nIl a en outre précisé que le monde serait à sa botte en moins de temps qu\'il n\'en fallait pour dire \"éléPHPant\". Pas dur, ceci dit entre nous...', '2017-09-20 16:28:41');
 
@@ -66,9 +70,9 @@ INSERT INTO `billets` (`id`, `titre`, `contenu`, `date_creation`) VALUES
 --
 
 --
--- Index pour la table `billets`
+-- Index pour la table `posts`
 --
-ALTER TABLE `billets`
+ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -76,10 +80,10 @@ ALTER TABLE `billets`
 --
 
 --
--- AUTO_INCREMENT pour la table `billets`
+-- AUTO_INCREMENT pour la table `posts`
 --
-ALTER TABLE `billets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
